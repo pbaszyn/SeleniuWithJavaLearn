cda41c3327909c7d6abc98c5662fd0779bbc0a8a
pbaszyn <61763299+pbaszyn@users.noreply.github.com>
2020-09-24 22:13:02 +0200

    New learnig course started

---
+++ start/src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    @Test
+    public void demoTest(){
+        WebDriver driver = new ChromeDriver();
+    }
+
+}
