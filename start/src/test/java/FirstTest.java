c9cc5a290d6c822053fabc00ed64be8f7e60a7e7
pbaszyn <61763299+pbaszyn@users.noreply.github.com>
2020-09-25 11:39:25 +0200

    First test rename

--- start/src/test/java/FirstTest.java
+++ start/src/test/java/FirstTest.java
@@ -4,8 +4,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
     @Test
-    public void demoTest(){
+    public void getTest(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+
+        driver.get("http://google.pl");
+
+        driver.close();
     }
 
 }
