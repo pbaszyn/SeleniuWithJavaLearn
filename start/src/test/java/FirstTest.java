3a73751f78727982c77e7417045395b8bf022541
pbaszyn <61763299+pbaszyn@users.noreply.github.com>
2020-09-25 11:47:17 +0200

    Added new test, small refactor

--- start/src/test/java/FirstTest.java
+++ start/src/test/java/FirstTest.java
@@ -1,16 +1,33 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
+    private WebDriver driver;
+
     @Test
     public void getTest(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
 
         driver.get("http://google.pl");
-
-        driver.close();
     }
 
+    @Test
+    public void navigateTest(){
+
+        driver.get("http://google.pl");
+    }
+
+    @BeforeEach
+    public void driverSetup(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void cleanAfterTest(){
+        driver.close();
+    }
 }
