9619b353a416b5cd9c2c1b391f613277cbdc87f4
pbaszyn <61763299+pbaszyn@users.noreply.github.com>
2020-11-06 01:11:28 +0100

    Added test of cookies number for loaded page

--- 9_CookieHomeWork/src/test/java/CookieHomeWorkTest.java
+++ 9_CookieHomeWork/src/test/java/CookieHomeWorkTest.java
@@ -1,11 +1,40 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Set;
 
 public class CookieHomeWorkTest {
 
     private WebDriver WebPageDriver;
+    private String WikiPageURL = "https://pl.wikipedia.org/";
+
+    @Test
+    public void shouldReturnCorrectNumberAfterPageLoad() {
+        //given
+        int ExpectedCookiesNumber = 4;
+
+        //when
+        WebPageDriver.navigate().to(WikiPageURL);
+        Set<Cookie> TestPageCookies = WebPageDriver.manage().getCookies();
+        waitOneSecond();
+
+        //then
+        Assertions.assertEquals(ExpectedCookiesNumber, TestPageCookies.size());
+    }
+
+    @Test
+    void shouldReturnTrueIfTestCookieWasAddedToTestPage() {
+        //given
+
+        //when
+
+        //then
+    }
 
     @BeforeEach
     public void WebPageDriverSetup(){
@@ -17,5 +46,13 @@ public class CookieHomeWorkTest {
     public void cleanAfterTest(){
         WebPageDriver.close();
         WebPageDriver.quit();
+    }
+
+    private void waitOneSecond(){
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
